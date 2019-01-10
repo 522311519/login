dbf43c7b2f933e49a0159bb1dd769fafb417fdce
522311519 <522311519@qq.com>
2019-01-10 09:38:57 +0800

    诈尸

--- src/com/xusheng/Load.java
+++ src/com/xusheng/Load.java
@@ -1,129 +1,125 @@
 package com.xusheng;
 
+import javax.swing.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
-import javax.swing.GroupLayout;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JTextField;
-import javax.swing.LayoutStyle;
-import javax.swing.SwingUtilities;
-import javax.swing.WindowConstants;
+/**
+ * @author xk
+ * @date 2019/1/10 09:28:13
+ * @description 登录
+ */
+public class Load extends JFrame {
 
-public class Load extends javax.swing.JFrame {
+    private static final long serialVersionUID = 1L;
+    private JTextField keyField;
+    private JButton load;
+    private JTextField nameField;
+    static ArrayList<User> user = new ArrayList<>();
 
-	private static final long serialVersionUID = 1L;
-	private JLabel userName;
-	private JLabel userKey;
-	private JTextField keyField;
-	private JButton sign, load;
-	private JTextField nameField;
-	static ArrayList<User> user = new ArrayList<User>();
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            Load inst = new Load();
+            inst.setLocationRelativeTo(null);
+            inst.setVisible(true);
+        });
+    }
 
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				Load inst = new Load();
-				inst.setLocationRelativeTo(null);
-				inst.setVisible(true);
-			}
-		});
-	}
+    public Load() {
+        super();
+        initGUI();
+    }
 
-	public Load() {
-		super();
-		initGUI();
-	}
-
-	private void initGUI() {
-		try {
-			GroupLayout thisLayout = new GroupLayout((JComponent) getContentPane());
-			getContentPane().setLayout(thisLayout);
-			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-			userName = new JLabel("用户名：");
-			userKey = new JLabel(" 密码：");
-			nameField = new JTextField();
-			nameField.setColumns(10);
-			keyField = new JTextField();
-			keyField.setColumns(16);
-			load = new JButton("登录");
-			load.addMouseListener(new MouseAdapter() {
-				@SuppressWarnings("unused")
-				public void mouseClicked(MouseEvent e) {
-					boolean flag = false;
-					for (User user1 : user) {
-						if (user1.name.equals(nameField.getText())) {
-							if (user1.key.equals(keyField.getText())) {
-								JOptionPane.showMessageDialog(load, "登陆成功！您的邮箱是：" + user1.email);
-								keyField.setText("");
-								nameField.setText("");
-							} else
-								JOptionPane.showMessageDialog(load, "密码错误！");
-						} else
-							JOptionPane.showMessageDialog(load, "用户不存在！");
-					}
-				}
-			});
-			{
-				sign = new JButton("注册");
-				sign.addMouseListener(new MouseAdapter() {
-					public void mouseClicked(MouseEvent e) {
-						ZhuCe inst = new ZhuCe();
-						inst.setLocationRelativeTo(null);
-						inst.setVisible(true);
-					}
-				});
-			}
-			thisLayout.setVerticalGroup(thisLayout.createSequentialGroup().addContainerGap(64, 64)
-					.addGroup(thisLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-							.addComponent(nameField, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE,
-									GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addComponent(userName, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE,
-									GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(29)
-					.addGroup(thisLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-							.addComponent(keyField, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE,
-									GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addComponent(userKey, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE,
-									GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(40)
-					.addGroup(thisLayout.createParallelGroup().addGroup(GroupLayout.Alignment.LEADING,
-							thisLayout.createSequentialGroup()
-									.addComponent(load, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE,
-											GroupLayout.PREFERRED_SIZE)
-									.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 0, Short.MAX_VALUE))
-							.addGroup(GroupLayout.Alignment.LEADING,
-									thisLayout.createSequentialGroup()
-											.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-											.addComponent(sign, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE,
-													GroupLayout.PREFERRED_SIZE)
-											.addGap(0, 0, Short.MAX_VALUE)))
-					.addContainerGap(62, 62));
-			thisLayout.setHorizontalGroup(thisLayout.createSequentialGroup().addContainerGap(66, 66)
-					.addGroup(thisLayout.createParallelGroup()
-							.addComponent(userName, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 53,
-									GroupLayout.PREFERRED_SIZE)
-							.addComponent(userKey, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 53,
-									GroupLayout.PREFERRED_SIZE))
-					.addGap(25)
-					.addGroup(thisLayout.createParallelGroup()
-							.addGroup(thisLayout.createSequentialGroup().addComponent(nameField,
-									GroupLayout.PREFERRED_SIZE, 167, GroupLayout.PREFERRED_SIZE))
-							.addGroup(thisLayout.createSequentialGroup().addComponent(keyField,
-									GroupLayout.PREFERRED_SIZE, 167, GroupLayout.PREFERRED_SIZE))
-							.addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
-									.addComponent(load, GroupLayout.PREFERRED_SIZE, 62, GroupLayout.PREFERRED_SIZE)
-									.addGap(43)
-									.addComponent(sign, GroupLayout.PREFERRED_SIZE, 62, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(81, Short.MAX_VALUE));
-			pack();
-			setSize(400, 300);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    private void initGUI() {
+        try {
+            GroupLayout thisLayout = new GroupLayout(getContentPane());
+            getContentPane().setLayout(thisLayout);
+            setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+            JLabel userName = new JLabel("用户名：");
+            JLabel userKey = new JLabel(" 密码：");
+            nameField = new JTextField();
+            nameField.setColumns(10);
+            keyField = new JTextField();
+            keyField.setColumns(16);
+            load = new JButton("登录");
+            load.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    boolean flag = false;
+                    for (User user1 : user) {
+                        if (user1.name.equals(nameField.getText())) {
+                            if (user1.key.equals(keyField.getText())) {
+                                JOptionPane.showMessageDialog(load, "登陆成功！您的邮箱是：" + user1.email);
+                                keyField.setText("");
+                                nameField.setText("");
+                            } else {
+                                JOptionPane.showMessageDialog(load, "密码错误！");
+                            }
+                        } else {
+                            JOptionPane.showMessageDialog(load, "用户不存在！");
+                        }
+                    }
+                }
+            });
+            JButton sign;
+            {
+                sign = new JButton("注册");
+                sign.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        ZhuCe inst = new ZhuCe();
+                        inst.setLocationRelativeTo(null);
+                        inst.setVisible(true);
+                    }
+                });
+            }
+            thisLayout.setVerticalGroup(thisLayout.createSequentialGroup().addContainerGap(64, 64)
+                    .addGroup(thisLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                            .addComponent(nameField, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE,
+                                    GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+                            .addComponent(userName, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE,
+                                    GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE))
+                    .addGap(29)
+                    .addGroup(thisLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                            .addComponent(keyField, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE,
+                                    GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+                            .addComponent(userKey, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE,
+                                    GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE))
+                    .addGap(40)
+                    .addGroup(thisLayout.createParallelGroup().addGroup(GroupLayout.Alignment.LEADING,
+                            thisLayout.createSequentialGroup()
+                                    .addComponent(load, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE,
+                                            GroupLayout.PREFERRED_SIZE)
+                                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 0, Short.MAX_VALUE))
+                            .addGroup(GroupLayout.Alignment.LEADING,
+                                    thisLayout.createSequentialGroup()
+                                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                            .addComponent(sign, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE,
+                                                    GroupLayout.PREFERRED_SIZE)
+                                            .addGap(0, 0, Short.MAX_VALUE)))
+                    .addContainerGap(62, 62));
+            thisLayout.setHorizontalGroup(thisLayout.createSequentialGroup().addContainerGap(66, 66)
+                    .addGroup(thisLayout.createParallelGroup()
+                            .addComponent(userName, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 53,
+                                    GroupLayout.PREFERRED_SIZE)
+                            .addComponent(userKey, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 53,
+                                    GroupLayout.PREFERRED_SIZE))
+                    .addGap(25)
+                    .addGroup(thisLayout.createParallelGroup()
+                            .addGroup(thisLayout.createSequentialGroup().addComponent(nameField,
+                                    GroupLayout.PREFERRED_SIZE, 167, GroupLayout.PREFERRED_SIZE))
+                            .addGroup(thisLayout.createSequentialGroup().addComponent(keyField,
+                                    GroupLayout.PREFERRED_SIZE, 167, GroupLayout.PREFERRED_SIZE))
+                            .addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
+                                    .addComponent(load, GroupLayout.PREFERRED_SIZE, 62, GroupLayout.PREFERRED_SIZE)
+                                    .addGap(43)
+                                    .addComponent(sign, GroupLayout.PREFERRED_SIZE, 62, GroupLayout.PREFERRED_SIZE)))
+                    .addContainerGap(81, Short.MAX_VALUE));
+            pack();
+            setSize(400, 300);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/com/xusheng/User.java
+++ src/com/xusheng/User.java
@@ -1,15 +1,21 @@
 package com.xusheng;
+/**
+ * @author xk
+ * @date 2019/1/10 09:38:18
+ * @description 用户
+ */
+public class User {
+    String name;
+    String key;
+    String email;
 
-public class User{
-	String name;
-	String key;
-	String email;
-	public User(){
-		
-	}
-	public User(String name,String key,String email){
-		this.name = name;
-		this.email = email;
-		this.key = key;
-	}
+    public User() {
+
+    }
+
+    public User(String name, String key, String email) {
+        this.name = name;
+        this.email = email;
+        this.key = key;
+    }
 }
